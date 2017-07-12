5edc07b02408da839a521241378ff4389e859cec
Clebson Derivan <cderivan@gmail.com>
2017-07-12 10:57:00 -0300

    genereate singed release

--- app/src/main/java/com/axiros/axactservice/MainActivity.java
+++ app/src/main/java/com/axiros/axactservice/MainActivity.java
@@ -1,13 +1,14 @@
 package com.axiros.axactservice;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.axiros.axact.AxirosService;
 
-public class MainActivity extends Activity  {
+public class MainActivity extends AppCompatActivity  {
     Intent mServiceIntent;
 
     @Override
@@ -17,11 +18,9 @@ public class MainActivity extends Activity  {
 
         mServiceIntent = new Intent(this, AxirosService.class);
         // About this key https://wiki.axiros.com/display/EFI/INTERNAL
-        mServiceIntent.putExtra("key", "KEY HERE");
+        mServiceIntent.putExtra("key", "YpZYIAXwSWP5zKpsVRp8eM5Y7dFl9RY+F07Q2OUdx7x1dvZMsiBBg6BqbGwGsWSS3VinmLFweXSepmIrCG1lgg==");
         startService(mServiceIntent);
 
         Toast.makeText(this, "AXACT Service started.", Toast.LENGTH_LONG).show();
-
-        finish();
     }
 }
